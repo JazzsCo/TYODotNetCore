184140135c6d2f03ebd5db604b0867c38ae37643
JazzsCo <122794596+JazzsCo@users.noreply.github.com>
2024-02-20 15:34:25 +0630

    Added WebApi CRUD

---
+++ TYODotNetCore.WebApi/Controllers/BlogController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TYODotNetCore.WebApi.Models;
+
+namespace TYODotNetCore.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly AppDbContext _db = new AppDbContext();
+
+        [HttpGet] 
+        public IActionResult GetBlogs()
+        {
+            List<BlogModel> lst = _db.Blogs.ToList();
+            return Ok(lst);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(BlogModel blogModel)
+        {
+            _db.Blogs.Add(blogModel);
+            int result = _db.SaveChanges();
+
+            if (result > 0)
+            {
+                return Ok("Create successful...");
+            }
+            else
+            {
+                return Ok("Create failed...");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlog(int id, BlogModel blogModel)
+        {
+            BlogModel? item = _db.Blogs.FirstOrDefault(item => item.BlogId == id);
+
+            if (item is null)
+            {
+                return NotFound("Data not found!!");
+            }
+
+            item.BlogTitle = blogModel.BlogTitle;
+            item.BlogAuthor = blogModel.BlogAuthor;
+            item.BlogContent = blogModel.BlogContent;
+
+            int result = _db.SaveChanges();
+
+            if (result > 0)
+            {
+                return Ok("Update successful...");
+            }
+            else
+            {
+                return Ok("Update failed...");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlog(int id)
+        {
+            BlogModel? item = _db.Blogs.FirstOrDefault(item => item.BlogId == id);
+
+            if (item is null)
+            {
+                return NotFound("Data not found!!");
+            }
+
+            _db.Blogs.Remove(item);
+            int result = _db.SaveChanges();
+
+            if (result > 0)
+            {
+                return Ok("Delete successful...");
+            }
+            else
+            {
+                return Ok("Delete failed...");
+            }
+        }
+    }
+}
